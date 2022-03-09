6e112b48c0563c208ce18a7a88779758966eac0d
david weinstein <davidmenachem464@gmail.com>
2022-03-09 21:14:49 +0200

    /

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Title from './Title'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
+        <Title/>
       </header>
     </div>
   );
---
+++ src/Title.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Title() {
+      let myName = ['david', 'weins', 19]
+      return (
+    myName.map((val) => <div>{val}</div>)
+
+  )
+}
